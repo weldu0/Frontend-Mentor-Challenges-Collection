c48fc36a9c4d75a7ae8ddce68e955f91ab12566c
woldu <16903781-woldu_lab@users.noreply.gitlab.com>
2024-08-04 02:14:36 +0100

    challenge FAQ accordion compeleted

---
+++ faq-accordion/README.md
@@ -0,0 +1,7 @@
+### Links
+
+- Solution URL: [click here](https://github.com/weldu0/Frontend-Mentor-Challenges-Collection/tree/main/faq-accordion)
+
+- Live Site URL: [click here](https://weldu0.github.io/Frontend-Mentor-Challenges-Collection/faq-accordion)
+
+- The challenge: [click here](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz)
---
+++ faq-accordion/main.js
@@ -0,0 +1,24 @@
+// // let questionAnswer = document.querySelector('.question-answer');
+// // let title = document.querySelector('.title');
+// // let answer = document.querySelector('.answer');
+
+
+// function showAnswer() {
+//   let answer = document.querySelector('.answer');
+//   let icon = document.querySelectorAll('.plus-icon');
+//   icon.forEach((icon) => icon.innerHTML = '<img src="assets/images/icon-plus.svg">')
+
+//   if (answer.style.display === 'none') {
+//     answer.style.display = 'block'
+//     icon.forEach((icon) => icon.innerHTML = '<img src="assets/images/icon-minus.svg">')
+
+//   } else {
+//     answer.style.display = 'none'
+    
+//   }
+// }
+
+// // icon to show at start of the page
+// // the function shoud
+// showAnswer()
+
